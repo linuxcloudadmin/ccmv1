cdfccbd7ae3522f5fde6f205291e47f3bfd23cd2
linuxcloudadmin <sudharsantcm@gmail.com>
2025-01-13 01:20:58 +0530

    commented error capture lines in setup proxy added and commented lines for docker internal host network. this can be uncommented when building a docker image.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,31 +1,32 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  // Error handler
-  const onError = (err, req, res) => {
-    console.error('Proxy error:', err.message);
+//   // Error handler
+//   const onError = (err, req, res) => {
+//     console.error('Proxy error:', err.message);
 
-    // Set response status and message
-    res.writeHead(500, {
-      'Content-Type': 'application/json',
-    });
-    res.end(
-      JSON.stringify({
-        error: 'Proxy error occurred. Please try again later.',
-        details: err.message,
-      })
-    );
-  };
+//     // Set response status and message
+//     res.writeHead(500, {
+//       'Content-Type': 'application/json',
+//     });
+//     res.end(
+//       JSON.stringify({
+//         error: 'Proxy error occurred. Please try again later.',
+//         details: err.message,
+//       })
+//     );
+//   };
 
   // Proxy for API 1
   app.use(
     '/api1', // Route to match
     createProxyMiddleware({
       target: 'http://localhost:8081',
+      // target: 'http://host.docker.internal:8081',
       changeOrigin: true,
       pathRewrite: { '^/api1': '' }, // Optional: Removes `/api1` from the request path
-      logLevel: 'debug',
-      onError,
+      // logLevel: 'debug',
+      // onError,
     })
   );
 
@@ -34,9 +35,10 @@ module.exports = function (app) {
     '/api2',
     createProxyMiddleware({
       target: 'http://localhost:8090',
+      // target: 'http://host.docker.internal:8090',
       changeOrigin: true,
       pathRewrite: { '^/api2': '' }, // Optional: Removes `/api2` from the request path
-      onError,
+      // onError,
     })
   );
 };
